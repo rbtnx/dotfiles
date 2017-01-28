ee37b63e9b3480ca097d64d0f58ae002eb3408f6
Kathrin <weihek@gmail.com>
2017-01-28 01:11:34 +0100

    spacemacs

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,32 +1,35 @@
-(require 'package)
+;;; init.el --- Spacemacs Initialization File
+;;
+;; Copyright (c) 2012-2016 Sylvain Benner & Contributors
+;;
+;; Author: Sylvain Benner <sylvain.benner@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
 
-; List the packages you want
-(setq package-list '(evil
-                     evil-leader))
+;; Without this comment emacs25 adds (package-initialize) here
+;; (package-initialize)
 
-; Add Melpa as the default Emacs Package repository
-; only contains a very limited number of packages
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+;; Increase gc-cons-threshold, depending on your system you may set it back to a
+;; lower value in your dotfile (function `dotspacemacs/user-config')
+(setq gc-cons-threshold 100000000)
 
-; Activate all the packages (in particular autoloads)
-(package-initialize)
+(defconst spacemacs-version          "0.200.7" "Spacemacs version.")
+(defconst spacemacs-emacs-min-version   "24.4" "Minimal version of Emacs.")
 
-; Update your local package index
-(unless package-archive-contents
-  (package-refresh-contents))
-
-; Install all missing packages
-(dolist (package package-list)
-  (unless (package-installed-p package)
-    (package-install package)))
-
-(require 'evil)
-(evil-mode t)
-
-(require 'evil-leader)
-(global-evil-leader-mode)
-(evil-leader/set-leader ",")
-(evil-leader/set-key
-  "b" 'switch-to-buffer
-  "w" 'save-buffer)
+(if (not (version<= spacemacs-emacs-min-version emacs-version))
+    (message (concat "Your version of Emacs (%s) is too old. "
+                     "Spacemacs requires Emacs version %s or above.")
+             emacs-version spacemacs-emacs-min-version)
+  (load-file (concat (file-name-directory load-file-name)
+                     "core/core-load-paths.el"))
+  (require 'core-spacemacs)
+  (spacemacs/init)
+  (spacemacs/maybe-install-dotfile)
+  (configuration-layer/sync)
+  (spacemacs-buffer/display-info-box)
+  (spacemacs/setup-startup-hook)
+  (require 'server)
+  (unless (server-running-p) (server-start)))
